da7ab73cfac7d71b96ab038666326af24f55b28e
Varnan Rathod <varnanrathod6@gmail.com>
2024-10-01 15:39:44 +0530

    Search functionality added.

--- src/main/java/com/hardvan/springboot/springolearn/controller/BookController.java
+++ src/main/java/com/hardvan/springboot/springolearn/controller/BookController.java
@@ -2,6 +2,7 @@ package com.hardvan.springboot.springolearn.controller;
 
 import com.hardvan.springboot.springolearn.model.Book;
 import com.hardvan.springboot.springolearn.service.BookService;
+import com.hardvan.springboot.springolearn.service.BookFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,6 +14,9 @@ import java.util.List;
 
 @Controller
 public class BookController {
+
+    @Autowired
+    private BookFilter bookFilter;
 
     @Autowired
     private BookService bookService;
@@ -34,5 +38,22 @@ public class BookController {
     public String deleteAllReviews(@RequestParam String title) {
         bookService.deleteAllReviews(title);
         return "redirect:/";
+    }
+
+    @GetMapping("/search")
+    public String searchBooks(@RequestParam String query, @RequestParam String searchType, Model model) {
+        List<Book> books = bookService.getAllBooks();
+        List<Book> filteredBooks;
+
+        if ("title".equals(searchType)) {
+            filteredBooks = bookFilter.filterBooksByTitle(books, query);
+        } else if ("genre".equals(searchType)) {
+            filteredBooks = bookFilter.filterBooksByGenre(books, query);
+        } else {
+            filteredBooks = books;
+        }
+
+        model.addAttribute("books", filteredBooks);
+        return "index";
     }
 }
